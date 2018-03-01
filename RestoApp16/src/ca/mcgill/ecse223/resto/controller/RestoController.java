20092ecfd25339b5682542698ca9e92f590f031f
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2018-02-28 19:50:51 -0500

    Added InvalidInputException class in controller package and deleteTable() method in RestoController.java

--- RestoApp16/src/ca/mcgill/ecse223/resto/controller/RestoController.java
+++ RestoApp16/src/ca/mcgill/ecse223/resto/controller/RestoController.java
@@ -1,5 +1,12 @@
 package ca.mcgill.ecse223.resto.controller;
 
-public class RestoController {
+import ca.mcgill.ecse223.resto.model.Table;
 
+public class RestoController {
+	public RestoController() {
+		
+	}
+	public static void deleteTable(Table table) {
+		table.delete();
+	}
 }
