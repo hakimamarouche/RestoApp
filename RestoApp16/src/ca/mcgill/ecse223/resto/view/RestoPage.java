34200832eaeac44c6293058c258cc7315c6b4aab
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2018-03-03 13:04:41 -0500

    Added table UI elements.

--- RestoApp16/src/ca/mcgill/ecse223/resto/view/RestoPage.java
+++ RestoApp16/src/ca/mcgill/ecse223/resto/view/RestoPage.java
@@ -1,6 +1,19 @@
 package ca.mcgill.ecse223.resto.view;
 
+import javax.swing.*;
+
 public class RestoPage {
+	
+	//UI elements
+	private JLabel errorMessage;
+	//table
+	private JTextField tableNumberTextField;
+	private JLabel tableNumberLabel;
+	private JLabel tableXLabel;
+	private JLabel tableYLabel;
+	private JComboBox<String> tableToggleList;
+	private JLabel tableToggleLabel;
+	private JButton deleteTableButton;
 
 	public void setVisible(boolean b) {
 		// TODO Auto-generated method stub
