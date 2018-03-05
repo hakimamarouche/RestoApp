bbd9acecbb5c7dcdd478e59f54901c85a13adbc2
Hakim <hakim.amarouche@mail.mcgill.com>
2018-03-05 17:17:37 -0500

    implemented createTable(), added test source folder, added one JUnit test

--- RestoApp16/src/ca/mcgill/ecse223/resto/controller/RestoController.java
+++ RestoApp16/src/ca/mcgill/ecse223/resto/controller/RestoController.java
@@ -10,11 +10,64 @@ import ca.mcgill.ecse223.resto.model.MenuItem;
 import ca.mcgill.ecse223.resto.model.MenuItem.ItemCategory;
 import ca.mcgill.ecse223.resto.model.Order;
 import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.model.Seat;
 import ca.mcgill.ecse223.resto.model.Table;
 
 public class RestoController {
 	public RestoController() {
 		
+	}
+	
+	public static void createTable (int number, int x, int y, int width, int length, int numberOfSeats) throws InvalidInputException {
+		String error = "";
+		if (x < 0 || y < 0) {
+			error = "The position of te table cannot be chararterised by negative x and y variables.";
+		}
+		if (number <= 0) {
+			error = "The number of a table has to be positive.";
+		}
+		if (width <= 0 || length <= 0) {
+			error = "The width and the length has to be positive.";
+		}
+		if (numberOfSeats <= 0) {
+			error = "The number of seats needs to be positive.";
+		}
+		if (error.length() > 0) {
+			throw new InvalidInputException(error.trim());
+		}
+		
+		RestoApp r = RestoApplication.getRestoApp();
+		List<Table> currentTables = r.getCurrentTables();
+		
+//		for (Table currentTable : currentTables) {
+//			try {
+//			}
+//			catch (RuntimeException e) {
+//				if (currentTable.doesOverlap()) {
+//					error = "Tables overlaps";
+//				}
+//			}
+//		}
+		
+		Table table;
+		try {
+		table = new Table(number, x, y, width, length, r);
+		}
+		catch (RuntimeException e) {
+			error = e.getMessage();
+			if (error.equals("Cannot create due to duplicate number")) {
+				error = "A table with this number already exists. Please use a different number.";
+			}
+			throw new InvalidInputException(error);
+		}
+		
+		r.addCurrentTable(table);
+		
+		for (int i = 1 ; i <= numberOfSeats; i++) {
+			Seat seat = table.addSeat();
+			table.addCurrentSeat(seat);
+		}
+//		RestoApplication.save();
 	}
 	
 	public static List<Table> getTables() {
